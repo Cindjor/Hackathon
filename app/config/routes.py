437f9ccf212ea2255df847708aedfc71c665bfd2
Cindjor <cheung.luke@gmail.com>
2015-10-19 22:51:39 -0700

    first test

--- app/config/routes.py
+++ app/config/routes.py
@@ -17,31 +17,21 @@ from system.core.router import routes
     The automatically generated routes respond to all of the http verbs (GET, POST, PUT, PATCH, DELETE)
 """
 routes['default_controller'] = 'Whats'
+#routes['/'] = 'Whats#index1'
 routes['/showgeocode']='Whats#getallgeocode'
-"""
-    You can add routes and specify their handlers as follows:
 
-    routes['VERB']['/URL/GOES/HERE'] = 'Controller#method'
 
-    Note the '#' symbol to specify the controller method to use.
-    Note the preceding slash in the url.
-    Note that the http verb must be specified in ALL CAPS.
-    
-    If the http verb is not provided pylot will assume that you want the 'GET' verb.
 
-    You can also use route parameters by using the angled brackets like so:
-    routes['PUT']['/users/<int:id>'] = 'users#update'
+routes['/events']= 'Whats#event'
 
-    Note that the parameter can have a specified type (int, string, float, path). 
-    If the type is not specified it will default to string
+routes['POST']['/create']='Whats#create_user'
 
-    Here is an example of the restful routes for users:
+routes['POST']['/create_event']='Whats#create_event'
 
-    routes['GET']['/users'] = 'users#index'
-    routes['GET']['/users/new'] = 'users#new'
-    routes['POST']['/users'] = 'users#create'
-    routes['GET']['/users/<int:id>'] = 'users#show'
-    routes['GET']['/users/<int:id>/edit' = 'users#edit'
-    routes['PATCH']['/users/<int:id>'] = 'users#update'
-    routes['DELETE']['/users/<int:id>'] = 'users#destroy'
-"""
+
+routes['/getcategorygeocode/<categoryid>'] = 'Whats#getcategorygeocode'
+
+routes['POST']['/login']='Whats#login_user'
+
+routes['/logout']='Whats#logout'
+
--- app/controllers/Whats.py
+++ app/controllers/Whats.py
@@ -4,23 +4,88 @@ import json
 class Whats(Controller):
 	def __init__(self, action):
 		super(Whats, self).__init__(action)
-
+		self.load_model('What')
 	def index(self):  
 		categories=self.getValueCategory()
-		print categories
+		session["userid"]= 1
 		return self.load_view('index.html',categories=categories)
 
+	
 	def getValueCategory(self):
 		category=[]
 		url = "http://api.eventful.com/json/categories/list?app_key=jk5zHMNmqkqjb6jS"
+		
 		res = json.loads(requests.get(url).content)
 
 		for dictvalue in res["category"]:
 			category.append({"id": dictvalue["id"], "name":dictvalue["name"].replace('&amp;','&')})
 		
 		return category
+
 	def getallgeocode(self):
-		url="http://api.eventful.com/json/events/search?app_key=jk5zHMNmqkqjb6jS&location=san%20jose,%20CA"
+		url="http://api.eventful.com/json/events/search?app_key=jk5zHMNmqkqjb6jS&location=san%20jose,%20CA&page_size=10"
 		res = requests.get(url).content
-		return res
+		return jsonify(res=res)
+
+	def getcategorygeocode(self,categoryid):
+		url="http://api.eventful.com/json/events/search?app_key=jk5zHMNmqkqjb6jS&location=san%20jose,%20CA&page_size=20"
+
+
+		url+=str(categoryid)
+		res = requests.get(url).content
+		return jsonify(res=res)
+
+	def event(self):
+		if "number" in session:
+			events= self.models['What'].get_events(session["id"])
+			return self.load_view('events.html', events=events)
+		else:
+			flash("User did not login")
+		return redirect("/")  
+
+	def login_user(self):
+		users_details={"number":request.form["number"],"password":request.form["pwd"]}
+		login_status=self.models['What'].login(users_details)
+		if login_status["status"]==True:
+			print login_status
+			session["name"]=str(login_status["userlogin"][0]["name"])
+			session["id"]=str(login_status["userlogin"][0]["id"])
+			session["number"]=str(login_status["userlogin"][0]["phone"])
+
+		else:
+			session.clear()
+			for message in login_status['error']:
+				flash(message, 'login_errors')
+
+		return redirect("/")   
+	        
+	def create_user(self):
+		users = {'name' : request.form['name'],'number' : request.form['number'],'password' : request.form['pwd'], 'pwd' : request.form['cpwd']}
+		store_user=self.models['What'].create_users(users)
+
+		if store_user["status"]==True:
+			session["id"] = store_user["userid"]
+			session["name"]= request.form["name"]
+			session["number"]= request.form["number"]
+			
+		else:
+			session.clear()
+			for message in store_user['error']:
+				flash(message)
+			
+		return redirect("/") 
+
+	
+	def create_event(self):
+	
+		input_events={"event_name":request.form["event_name"],"date":request.form["start_time"].split(" ")[0],"time":request.form["start_time"].split(" ")[1],"place":request.form["venue_name"]}
+		self.models['What'].create_event(input_events, session["id"])
+
+		return redirect('/')
+
+	def logout(self):
+		print 'logout'
+		session.clear()
+		return redirect ('/')
+
 
